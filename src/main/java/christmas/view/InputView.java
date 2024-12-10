33b68db8a656cd345b844fb87be0d5f9bd36a639
openmpy <suhwan@kakao.com>
2024-12-10 16:27:06 +0900

    refactor: 방문 예약 일자 예외 처리 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,14 +1,19 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.exception.InvalidVisitDateException;
 import christmas.view.constants.ViewMessage;
 
 public class InputView {
 
     public static int readVisitDate() {
-        System.out.println(ViewMessage.INPUT_VISIT_DATE.getValue());
-        String value = Console.readLine();
-        return Integer.parseInt(value);
+        try {
+            System.out.println(ViewMessage.INPUT_VISIT_DATE.getValue());
+            String value = Console.readLine();
+            return Integer.parseInt(value);
+        } catch (IllegalArgumentException e) {
+            throw new InvalidVisitDateException();
+        }
     }
 
     public static String readOrderMenu() {
