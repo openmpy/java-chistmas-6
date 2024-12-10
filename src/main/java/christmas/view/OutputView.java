85fac221e5e97009222f05e9ef9793b3fbcfb98e
openmpy <suhwan@kakao.com>
2024-12-10 15:02:25 +0900

    feat: 주문 메뉴 목록 메세지 출력 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,10 +1,24 @@
 package christmas.view;
 
+import christmas.domain.OrderMenus;
+import christmas.domain.VisitDate;
 import christmas.view.constants.ViewMessage;
 
 public class OutputView {
 
     public static void printWelcome() {
         System.out.println(ViewMessage.OUTPUT_WELCOME.getValue());
+    }
+
+    public static void printEventPreview(VisitDate visitDate) {
+        String formatted = String.format(ViewMessage.OUTPUT_EVENT_PREVIEW.getValue(), visitDate.getDate());
+        System.out.println(formatted);
+        System.out.println();
+    }
+
+    public static void printOrderMenus(OrderMenus orderMenus) {
+        System.out.println(ViewMessage.OUTPUT_ORDER_MENU_HEADER.getValue());
+        orderMenus.printMenus();
+        System.out.println();
     }
 }
--- src/main/java/christmas/view/constants/ViewMessage.java
+++ src/main/java/christmas/view/constants/ViewMessage.java
@@ -6,7 +6,7 @@ public enum ViewMessage {
     INPUT_ORDER_MENU("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
 
     OUTPUT_WELCOME("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
-    OUTPUT_EVENT_PREVIEW("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
+    OUTPUT_EVENT_PREVIEW("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     OUTPUT_ORDER_MENU_HEADER("<주문 메뉴>"),
     OUTPUT_BEFORE_DISCOUNT_HEADER("<할인 전 총주문 금액>"),
     OUTPUT_BONUS_FOOD_HEADER("<증정 메뉴>"),
