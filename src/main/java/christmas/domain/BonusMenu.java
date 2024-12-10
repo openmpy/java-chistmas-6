0f8170341ba90ec9068702b1ff81edd76d5d9e43
openmpy <suhwan@kakao.com>
2024-12-10 15:19:48 +0900

    feat: 증정 메뉴 관련된 도메인 추가

---
+++ src/main/java/christmas/domain/BonusMenu.java
@@ -0,0 +1,40 @@
+package christmas.domain;
+
+import christmas.domain.constants.FoodInfo;
+import christmas.domain.constants.FoodType;
+import christmas.exception.InvalidOrderMenuException;
+
+public class BonusMenu {
+
+    private final FoodInfo foodInfo;
+    private final int amount;
+
+    public BonusMenu(String foodName, int amount) {
+        validateAmount(amount);
+
+        this.foodInfo = FoodInfo.of(foodName);
+        this.amount = amount;
+    }
+
+    private void validateAmount(int amount) {
+        if (amount < 1) {
+            throw new InvalidOrderMenuException();
+        }
+    }
+
+    public String getFoodName() {
+        return foodInfo.getName();
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public FoodType getFoodType() {
+        return foodInfo.getType();
+    }
+
+    public int getPrice() {
+        return foodInfo.getPrice();
+    }
+}
