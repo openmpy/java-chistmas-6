d385f974032f3d77943ab5733be547f3e42d9440
openmpy <suhwan@kakao.com>
2024-12-10 16:12:20 +0900

    feat: 이벤트 메세지 출력 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,6 @@
 package christmas.view;
 
+import christmas.domain.Event;
 import christmas.domain.OrderMenus;
 import christmas.domain.VisitDate;
 import christmas.view.constants.ViewMessage;
@@ -26,5 +27,34 @@ public class OutputView {
         System.out.println(ViewMessage.OUTPUT_BEFORE_DISCOUNT_HEADER.getValue());
         orderMenus.printBeforeDiscountTotalPrice();
         System.out.println();
+    }
+
+    public static void printBonusMenu(Event event) {
+        System.out.println(ViewMessage.OUTPUT_BONUS_FOOD_HEADER.getValue());
+        event.printBonusMenu();
+        System.out.println();
+    }
+
+    public static void printEvent(Event event) {
+        System.out.println(ViewMessage.OUTPUT_EVENT_LIST_HEADER.getValue());
+        event.printEvents();
+        System.out.println();
+    }
+
+    public static void printTotalDiscountPrice(Event event) {
+        System.out.println(ViewMessage.OUTPUT_TOTAL_DISCOUNT_HEADER.getValue());
+        event.printTotalDiscountPrice();
+        System.out.println();
+    }
+
+    public static void printAfterTotalDiscountPrice(Event event, OrderMenus orderMenus) {
+        System.out.println(ViewMessage.OUTPUT_AFTER_DISCOUNT_HEADER.getValue());
+        event.printAfterTotalDiscountPrice(orderMenus);
+        System.out.println();
+    }
+
+    public static void printBadge(Event event) {
+        System.out.println(ViewMessage.OUTPUT_BADGE_HEADER.getValue());
+        event.printBadge();
     }
 }
